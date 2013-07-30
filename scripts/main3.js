3cf31772129a748c66e1afacade350794ab99145
chrisw <chrisw@soe.ucsc.edu>
2013-07-30 16:57:49 -0700

    Button adds new node, but graph becomes unresponsive to mouse events.

--- scripts/main3.js
+++ scripts/main3.js
@@ -5,6 +5,7 @@ var charge = -100;
 var linkDistance = 100;
 var nodeRadius = 20;
 var dataURL = "data/net";
+var friction = 0.3
 
 var color = d3.scale.category20();
 
@@ -12,22 +13,14 @@ var force = d3.layout.force().charge(charge).linkDistance(linkDistance).size([wi
 
 var form = d3.select("body").append("form");
 
-form.append("input").attr({
-    id: "button",
-    type : "button",
-    value : "test button",
-    name : "testButton"
-}).on("click", function() {
-    id = this.getAttribute("id");
-    value = this.getAttribute("value");
-    alert(id + ' clicked: ' + value);
-    return true;
+var svg = d3.select("body").append("svg").attr({
+    'width' : width,
+    'height' : height
 });
-
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 // type="button" value="See Some Text" name="button2" onClick="window.status='You clicked the button!'; return true"
 
 d3.json(dataURL, function(error, data) {
+
     if (error !== null) {
         console.log("error --> " + error);
     } else {
@@ -37,45 +30,75 @@ d3.json(dataURL, function(error, data) {
     var nodes = graph.nodes;
     var links = graph.links;
 
-    // start the layout
-    force.nodes(nodes).links(links).start();
+    function createNode(name, group) {
+        newNode = new Object();
+        newNode['name'] = name;
+        newNode['group'] = group;
+        return newNode;
+    }
 
-    // links
-    var link = svg.selectAll(".link").data(links).enter().append("line").attr("class", "link").style("stroke-width", function(d) {
-        return d.value;
-    });
+    function setupLayout() {
+        // start the layout
+        force.friction(friction).nodes(nodes).links(links).start();
 
-    // nodes
-    var node = svg.selectAll(".node").data(nodes).enter().append("circle").attr("class", "node").attr("r", nodeRadius).style("fill", function(d) {
-        return color(d.group);
-    }).call(force.drag);
-
-    // tooltips
-    link.append("title").text(function(d) {
-        var label = d.source.name + "-->" + d.target.name + ":" + d.value;
-        return label;
-    });
-
-    node.append("title").text(function(d) {
-        return d.name;
-    });
-
-    // tick handler repositions graph elements
-    force.on("tick", function() {
-        link.attr("x1", function(d) {
-            return d.source.x;
-        }).attr("y1", function(d) {
-            return d.source.y;
-        }).attr("x2", function(d) {
-            return d.target.x;
-        }).attr("y2", function(d) {
-            return d.target.y;
+        // links
+        var link = svg.selectAll(".link").data(links).enter().append("line").attr("class", "link").style("stroke-width", function(d) {
+            return d.value;
         });
 
-        node.attr("cx", function(d) {
-            return d.x;
-        }).attr("cy", function(d) {
-            return d.y;
+        // nodes
+        var node = svg.selectAll(".node").data(nodes).enter().append("circle").attr("class", "node").attr("r", nodeRadius).style("fill", function(d) {
+            return color(d.group);
+        }).call(force.drag);
+
+        // tooltips
+        link.append("title").text(function(d) {
+            var label = d.source.name + "-->" + d.target.name + ":" + d.value;
+            return label;
         });
+
+        node.append("title").text(function(d) {
+            return d.name;
+        });
+
+        // tick handler repositions graph elements
+        force.on("tick", function() {
+            link.attr("x1", function(d) {
+                return d.source.x;
+            }).attr("y1", function(d) {
+                return d.source.y;
+            }).attr("x2", function(d) {
+                return d.target.x;
+            }).attr("y2", function(d) {
+                return d.target.y;
+            });
+
+            node.attr("cx", function(d) {
+                return d.x;
+            }).attr("cy", function(d) {
+                return d.y;
+            });
+        });
+    }
+
+
+    form.append("input").attr({
+        id : "button",
+        type : "button",
+        value : "test button",
+        name : "testButton"
+    }).on("click", function() {
+        id = this.getAttribute("id");
+        value = this.getAttribute("value");
+
+        newNode = createNode(Math.random().toString(), 5);
+
+        console.log('old size: ' + nodes.length);
+        nodes.push(newNode);
+        console.log('new size: ' + nodes.length);
+
+        setupLayout();
+
+        return true;
     });
 });
