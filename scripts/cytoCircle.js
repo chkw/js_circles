c0e81ee6df8d9bab2410fbacecb3d2419be8f69c
chrisw <chrisw@soe.ucsc.edu>
2015-02-11 12:55:28 -0800

    spring embeded layout

--- scripts/cytoCircle.js
+++ scripts/cytoCircle.js
@@ -63,14 +63,86 @@ var cytoCircle = {};
 
             // http://js.cytoscape.org/#layouts
             'layout' : {
-                name : 'breadthfirst',
-                directed : true,
-                padding : 10
-            },
 
-            // layout : {
-            // name : 'cose'
-            // },
+                // 'name' : 'breadthfirst',
+                // 'fit' : true, // whether to fit the viewport to the graph
+                // 'directed' : true, // whether the tree is directed downwards (or edges can point in any direction if false)
+                // 'padding' : 10, // padding on fit
+                // 'circle' : false, // put depths in concentric circles if true, put depths top down if false
+                // 'boundingBox' : undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+                // 'avoidOverlap' : true, // prevents node overlap, may overflow boundingBox if not enough space
+                // 'roots' : undefined, // the roots of the trees
+                // 'maximalAdjustments' : 3, // how many times to try to position the nodes in a maximal way (i.e. no backtracking)
+                // 'animate' : false, // whether to transition the node positions
+                // 'animationDuration' : 500, // duration of animation in ms if enabled
+                // 'ready' : function() {
+                // console.log('layout ready');
+                // }, // callback on layoutready
+                // 'stop' : function() {
+                // console.log('layout stop');
+                // } // callback on layoutstop
+
+                name : 'cose',
+
+                // Called on `layoutready`
+                ready : function() {
+                },
+
+                // Called on `layoutstop`
+                stop : function() {
+                },
+
+                // Whether to animate while running the layout
+                animate : false,
+
+                // Number of iterations between consecutive screen positions update (0 -> only updated on the end)
+                refresh : 4,
+
+                // Whether to fit the network view after when done
+                fit : true,
+
+                // Padding on fit
+                padding : 10,
+
+                // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+                boundingBox : undefined,
+
+                // Whether to randomize node positions on the beginning
+                randomize : true,
+
+                // Whether to use the JS console to print debug messages
+                debug : false,
+
+                // Node repulsion (non overlapping) multiplier
+                nodeRepulsion : 400000,
+
+                // Node repulsion (overlapping) multiplier
+                nodeOverlap : 10,
+
+                // Ideal edge (non nested) length
+                idealEdgeLength : 10,
+
+                // Divisor to compute edge forces
+                edgeElasticity : 100,
+
+                // Nesting factor (multiplier) to compute ideal edge length for nested edges
+                nestingFactor : 5,
+
+                // Gravity force (constant)
+                gravity : 250,
+
+                // Maximum number of iterations to perform
+                numIter : 100,
+
+                // Initial temperature (maximum node displacement)
+                initialTemp : 200,
+
+                // Cooling factor (how the temperature is reduced between consecutive iterations
+                coolingFactor : 0.95,
+
+                // Lower temperature threshold (below this point the layout will end)
+                minTemp : 1.0
+            },
 
             'style' : cytoscape.stylesheet().selector('node').css({
                 'height' : 80,
